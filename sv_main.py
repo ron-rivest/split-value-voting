bdb6db4c245b8979b1c3606237f0db616c2d6a58
ron-rivest <rivest@mit.edu>
2014-06-24 14:16:03 -0400

    added ability to have election parameters in a file.

--- sv_main.py
+++ sv_main.py
@@ -5,7 +5,11 @@
 """
 Top-level routine for running simulated election using split-value method.
 
-Usage: python3 sv_main.py
+Usage: 
+        python3 sv_main.py
+  or    
+        python3 sv_main.py election_id
+        where election description is given in election_id.parameters.txt
 """
 
 ##############################################################################
@@ -39,35 +43,51 @@ THE SOFTWARE.
 # end of standard MIT open-source license
 ##############################################################################
 
+import json
 import sys
 assert sys.version_info[0] == 3
 
 import sv_election
 import sv_verifier
 
+default_election_parameters = {
+    "election_id": "test01",
+    "ballot_style":\
+    [("taxes", ("yes", "no")),
+     ("mayor", ("tom",
+                "rufus",
+                "****************"))],  # 16-char write-ins allowed
+    "n_voters": 3,       # voters
+    "n_reps": 4,         # (# of replicas aka 2m)
+    "n_fail": 1,         # how many servers may fail
+    "n_leak": 1,         # how many servers may leak
+
+    # optional parameters:
+    # number of hex digits in ballot id (default 32)
+    "ballot_id_len": 32,
+    # number of spaces per tab in json output (>=0, default 0)
+    # setting this to 0 reduces readability of SBB output, but
+    # also reduces SBB size by roughly 25%
+    "json_indent": 1
+}
+
+def get_election_parameters():
+    """ Get election parameters if available, from a file. 
+        Else use default.
+    """
+    election_parameters = default_election_parameters
+    if len(sys.argv) > 1:
+        election_id = sys.argv[1]
+        election_parameter_filename = election_id + ".parameters.txt"
+        election_parameter_file = open(election_parameter_filename,'r')
+        election_parameters = json.load(election_parameter_file)
+        election_parameter_file.close()
+    return election_parameters
+
 def do_election():
     """ Do (simulate) an election. """
 
-    election_parameters = {
-        "election_id": "test01",
-        "ballot_style":\
-            [# ("taxes", ("yes", "no")),
-             ("mayor", ("tom",
-                        "rufus",
-                        "****************"))],  # 16-char write-ins allowed
-        "n_voters": 3,       # voters
-        "n_reps": 4,         # (# of replicas aka 2m)
-        "n_fail": 1,         # how many servers may fail
-        "n_leak": 1,         # how many servers may leak
-
-        # optional parameters:
-        # number of hex digits in ballot id (default 32)
-        "ballot_id_len": 32,
-        # number of spaces per tab in json output (>=0, default 0)
-        # setting this to 0 reduces readability of SBB output, but
-        # also reduces SBB size by roughly 25%
-        "json_indent": 1
-    }
+    election_parameters = get_election_parameters()
     print("starting election (simulation).")
     print("election parameters:")
     for key in sorted(election_parameters.keys()):
